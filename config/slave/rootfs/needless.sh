3fbfbf7972364289562623d6366d2a58c56a86ba
dimmalex <dimmalex@gmail.com>
2025-06-28 18:08:10 +0800

    don't install the he and daemon to bin for compatibility tiger7

--- config/slave/rootfs/needless.sh
+++ config/slave/rootfs/needless.sh
@@ -6,6 +6,9 @@ rm -fr ./usr/share/skinos/ifname
 rm -fr ./usr/share/skinos/network
 # delete clock
 rm -fr ./usr/share/skinos/clock
+# delete execute bin for compatibility tiger7
+rm -fr ./usr/local/bin/daemon
+rm -fr ./usr/local/bin/he
 
 # delete self
 rm -fr ./needless.sh
